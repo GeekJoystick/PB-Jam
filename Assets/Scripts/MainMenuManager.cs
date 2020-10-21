fd3dd81c865c9f74b715974a23ed7a20033ca5aa
Lord Yamoshi <babbas107@gmail.com>
2020-10-22 01:19:24 +0200

    UI For almost everything
    Main Menu, Pause Menu, Credits Menu is done

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+ 
+	public void OnPlay()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	public void OnEndless()
+	{
+		SceneManager.LoadScene(3);
+	}
+
+	public void OnCredits()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void OnExit()
+	{
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			Invoke(nameof(DestroyThis), 5f);
+		}
+
+		if(collision.gameObject.tag == "Player")
+		{
+			/*Health health = collision.gameObject.GetComponent<Health>();
+			if (health)
+			{
+				health.Hurt();
+			}
+			Destroy(gameObject);*/
+		}
+
+	}
+
+
+	public void DestroyThis()
+	{
+		Destroy(gameObject);
+	}
+
+}
