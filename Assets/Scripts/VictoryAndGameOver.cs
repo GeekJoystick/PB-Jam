e6427a1e20e48499eb82ea3e2659159ae98be5ec
Lord Yamoshi <babbas107@gmail.com>
2020-10-22 16:02:43 +0200

    GameOver/Victory

---
+++ Assets/Scripts/VictoryAndGameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryAndGameOver : MonoBehaviour
+{
+
+	public void OnTryAgainAndPlayAgain()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+
+	public void OnMainMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void OnQuit()
+	{
+		Application.Quit();
+	}
+}
